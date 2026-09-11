d7ef3fe32ae54583c801e3fc01179806deb1117d
Akhil Varma <69309712+DAV027@users.noreply.github.com>
2026-09-11 21:10:26 +0530

    feat: expose experiment catalog API

---
+++ xau_lab/experiments/__init__.py
@@ -0,0 +1,10 @@
+from xau_lab.experiments.sampler import FAMILY_BUDGET_WEIGHTS, generate_catalog
+from xau_lab.experiments.spec import CompleteExperiment, canonical_fingerprint, canonical_json
+
+__all__ = [
+    "CompleteExperiment",
+    "canonical_fingerprint",
+    "canonical_json",
+    "FAMILY_BUDGET_WEIGHTS",
+    "generate_catalog",
+]
