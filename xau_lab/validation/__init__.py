0358698c15c4da3b62a33070c5441eeee48b06cb
Akhil Varma <69309712+DAV027@users.noreply.github.com>
2026-09-12 10:57:01 +0530

    feat: initialize validation package

---
+++ xau_lab/validation/__init__.py
@@ -0,0 +1,5 @@
+"""Robustness validation and candidate promotion helpers."""
+
+from xau_lab.validation.promotion import PromotionDecision, stage1_decision
+
+__all__ = ["PromotionDecision", "stage1_decision"]
